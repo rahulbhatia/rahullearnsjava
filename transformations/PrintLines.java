a4a865801a69a99de01e98d1ef46ffc839fc292f
Rahul Bhatia <rahul@budapest.local>
2018-10-09 12:10:04 -0400

    Adding PrintLines initial project

---
+++ transformations/PrintLines.java
@@ -0,0 +1,41 @@
+// PrintLines
+
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
+public class PrintLines {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a file name: ");
+
+        String filename = scanner.next();
+
+        System.out.println("Processing " + filename);
+
+        processFile(filename);
+
+    }
+    public static void processFile(String filename) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));  
+            String line = null; 
+            while ((line = br.readLine()) != null)  
+            {  
+                System.out.println(line);
+            } 
+        }
+        catch(FileNotFoundException f) {
+            System.out.println("File Not Found");
+            return;
+        }
+        catch(java.io.IOException e) {
+            System.out.println("Caught an IOException");
+            return;
+        }
+        
+    }
+}
---
+++ transformations/README.md
@@ -0,0 +1,9 @@
+# String Manipulation
+
+This project is based on the MP1 from University of Illinois' CS125 (the first Java class I could think of). 
+
+Link: https://cs125.cs.illinois.edu/MP/1/
+
+## Tasks
+  - Process a file line by line
+  - Read a Csv File
